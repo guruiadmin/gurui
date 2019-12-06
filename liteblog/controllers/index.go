c783549ff42ca37f638674926776a8c80ba58a09
wpf <1191534583@qq.com>
2019-12-06 18:12:14 +0800

    update

--- liteblog/controllers/index.go
+++ liteblog/controllers/index.go
@@ -6,6 +6,18 @@ type IndexController struct {
 	beego.Controller
 }
 
+//@router / [get]
 func (this *IndexController) Index(){
 	this.TplName = "index.html"
+}
+
+//@router /message [get]
+func (this *IndexController) IndexMessage(){
+	this.TplName = "message.html"
+}
+
+
+//@router /about [get]
+func (this *IndexController) IndexAbout(){
+	this.TplName = "about.html"
 }
